8cf955b16fd68a79e1455ec28916dff3d232358e
Ankur Oberoi <aoberoi@gmail.com>
2015-06-22 01:58:44 -0700

    adding a version that builds on all platforms
    successfully fetches and includes opentok.js from web

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-opentokjs/src/browser/opentok.js",
+        "id": "cordova-plugin-opentokjs.OpenTokClient",
+        "clobbers": [
+            "OT"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-opentokjs": "0.1.0",
+    "cordova-plugin-crosswalk-webview": "1.2.0"
 }
 // BOTTOM OF METADATA
 });
