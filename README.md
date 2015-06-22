ca1d77d9b135e5840e96dfc35299321851079cc1
Ankur Oberoi <aoberoi@gmail.com>
2015-06-22 03:13:03 -0700

    adds basic build instructions, implements a 1 to 1 opentok session

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hello OpenTokJS
+
+This is an example application which shows the use of the `cordova-plugin-opentokjs` plugin for
+Cordova.
+
+## Setup
+
+You will need an OpenTok API Key, Session ID, and Token. If you do not already have these, you can
+get these values from the [TokBox Dashboard](https://dashboard.tokbox.com).
+
+1. Rename the `www/js/config.js.sample` file to `www/js/config.js`.
+
+2. Insert your own values for your OpenTok API Key, Session ID, and a Token into `www/js/config.js`.
+
+3. Build: `$ cordova build`.
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,51 +1,65 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  // State
+  config: undefined,
+  session: undefined,
+  publisher: undefined,
+  subscriber: undefined,
+  isSubscribing: false,
 
-        console.log('Received Event: ' + id);
+  // Application Constructor
+  initialize: function() {
+    this.config = cordova.require('config');
+    this.bindEvents();
+  },
+
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    OT.setLogLevel(OT.DEBUG);
+    app.initializePublisher();
+    app.initializeSession();
+  },
+
+  initializePublisher: function() {
+    app.publisher = OT.initPublisher('publisher');
+  },
+
+  initializeSession: function() {
+    app.session = OT.initSession(app.config.apiKey, app.config.sessionId);
+    app.session.on('streamCreated', app.onStreamCreated);
+    app.session.on('streamDestroyed', app.onStreamDestroyed);
+    app.session.connect(app.config.token, app.onSessionConnected);
+  },
+
+  onSessionConnected: function(event) {
+    app.session.publish(app.publisher);
+  },
+
+  onStreamCreated: function(event) {
+    if (!app.isSubscribing) {
+      app.subscriber = app.session.subscribe(event.stream, 'subscriber');
+      app.isSubscribing = true;
     }
+  },
+
+  onStreamDestroyed: function(event) {
+    if (app.isSubscribing && event.stream.streamId === app.subscriber.stream.streamId) {
+      app.session.unsubscribe(app.subscriber);
+      app.isSubscribing = false;
+      app.subscriber = undefined;
+    }
+  }
 };
 
 app.initialize();
